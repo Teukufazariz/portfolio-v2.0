d4f0b460f2f6b2b0c205effc9027da3695f8f944
Teukufazariz <teukufazarizbasya@gmail.com>
2026-01-08 14:50:10 +0700

    hotfix: vercel speed analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Teuku Fazariz B.",
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className="antialiased">{children}</body>
       <Analytics />
+      <SpeedInsights/>
     </html>
   );
 }
